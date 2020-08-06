796b19bf16a43dc74b17488225bc5884fe35373b
MYMS2020 <2112717099@qq.com>
2020-08-06 19:30:51 +0800

    Auto push to git

--- gitpush.sh
+++ gitpush.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
-#D:\RJ\Git\git-bash.exe
+#git config --global user.name "MYMS2020"
+#git config --global user.email "2112717099@qq.com" 
+#git remote add origen git@github.com:MYMS2020/MYMS2020.github.io 
+git branch
 git add .
-read message
-git commit -m "$message"
-git push -u origin master
+git status
+git commit -m "Auto push to git"
+git push -u origin master:master
+echo 9
+sleep 1
+echo 8
+sleep 1
+echo 7
+sleep 1
+echo 6
+sleep 1
+echo 5
+sleep 1
+echo 4
+sleep 1
+echo 3
+sleep 1
+echo 2
+sleep 1
+echo 1
+sleep 1
